8a4bd116e13480192e3503bb238e29d6c0925ffe
amr aboelnaga <amraboelnaga@amrs-Mac-mini.local>
2024-07-07 15:03:27 -0400

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
-import { Amplify } from "aws-amplify";n
-import awsconfig from "./aws-exports";n
-Amplify.configure(awsconfig);n
+import { Amplify } from "aws-amplify";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import config from './aws-exports';
+
+
+
+Amplify.configure(config);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
