13f307e71c6d99de2c85f6085531e8b2ca2ccadc
Mahmoud Dahab <mdahab@Mahmouds-Mac-mini.local>
2024-09-12 11:55:00 +0300

    bold italic underlin strickthrough per selected text

--- src/components/whiteboard-components/EditableText.tsx
+++ src/components/whiteboard-components/EditableText.tsx
@@ -1,152 +1,138 @@
-import React, { useEffect, useRef } from 'react';
-import { observer } from 'mobx-react-lite';
-import { Text } from '../../types/whiteBoard';
-
+import React, { useEffect, useRef, useState } from "react";
+import { observer } from "mobx-react-lite";
+import { Text } from "../../types/whiteBoard";
 
 interface EditableTextProps {
-    textData: Text;
-    isEditing: boolean;
-    onChange: (newValue: string) => void;
-    onFocus: () => void;
-    onBlur: (event: React.FocusEvent) => void;
-    type?: string;
-    pageNumbHelper?: number;
-    order?: number
-
+  textData: Text;
+  isEditing: boolean;
+  onChange: (newValue: string) => void;
+  onFocus: () => void;
+  onBlur: (event: React.FocusEvent) => void;
+  type?: string;
+  pageNumbHelper?: number;
+  order?: number;
 }
 
-export const EditableText: React.FC<EditableTextProps> = observer(({
-    textData,
-    isEditing,
-    onChange,
-    onFocus,
-    onBlur,
-    type,
-    pageNumbHelper,
-    order
-}) => {
-    const textareaRef = useRef<HTMLTextAreaElement>(null);
-    const [focused, setFocused] = React.useState(false)
-    useEffect(() => {
-        if (textareaRef.current) {
-            textareaRef.current.style.height = textData.fontSize;
-            textareaRef.current.style.height = (textareaRef.current.scrollHeight) + 'px';
-        }
-    }, [isEditing, textData, onBlur]);
+export const EditableText: React.FC<EditableTextProps> = observer(({ textData, isEditing, onChange, onFocus, onBlur, type, pageNumbHelper, order }) => {
+  const editableDivRef = useRef<HTMLDivElement>(null);
+  const [focused, setFocused] = useState(false);
 
-    return (
-        <div style={{ ...styles.EditableTextCont }}>
-            <p style={{
-                ...styles.number,
-                display: type !== 'List' ? 'none' : '',
-                fontSize: textData.fontSize,
-                fontWeight: textData.fontWeight,
-                fontStyle: textData.fontStyle,
-                color: textData.color,
-                textDecoration: textData.textDecoration,
-                textAlign: textData.textAlign,
-                justifyContent: textData.textAlign,
-                letterSpacing: textData.letterSpacing,
-                lineHeight: textData.lineHeight,
-                fontFamily: textData.fontFamily,
-                textTransform: textData.textTransform,
-                whiteSpace: textData.whiteSpace,
-                wordBreak: textData.wordBreak,
-            }}>{order ? `${order}-` : type === 'List' && !order ? '-' : ""} </p>
-            {isEditing && type !== 'PageNumber' ? (
-                <textarea
-                    wrap="no-wrap"
-                    name="content"
-                    value={`${textData.content}`}
-                    onFocus={() => {
-                        onFocus();
-                        setFocused(true)
-                    }}
-                    onBlur={(e) => {
-                        //onBlur(e);
-                        setFocused(false)
-                    }}
-                    onChange={(e) => {
-                        onChange(e.target.value);
-                        e.target.style.height = e.target.scrollHeight + 'px';
-                    }}
+  useEffect(() => {
+    if (editableDivRef.current) {
+      editableDivRef.current.innerHTML = textData.content; // Ensure the initial content is set
+    }
+  }, [textData.content]);
 
-                    ref={textareaRef}
-                    style={{
-                        ...styles.textareaStyle,
-                        fontSize: textData.fontSize,
-                        fontWeight: textData.fontWeight,
-                        fontStyle: textData.fontStyle,
-                        color: textData.color,
-                        textDecoration: textData.textDecoration,
-                        textAlign: textData.textAlign,
-                        justifyContent: textData.textAlign,
-                        letterSpacing: textData.letterSpacing,
-                        lineHeight: textData.lineHeight,
-                        fontFamily: textData.fontFamily,
-                        textTransform: textData.textTransform,
-                        whiteSpace: textData.whiteSpace,
-                        wordBreak: textData.wordBreak,
-                        boxShadow: focused ? 'inset 0 0 0 1px blue' : 'none',
-                        height: textData.fontSize
-                    }}
-                />
-            ) : (
-                <p style={{
-                    ...styles.text,
-                    fontSize: textData.fontSize,
-                    fontWeight: textData.fontWeight,
-                    fontStyle: textData.fontStyle,
-                    color: textData.color,
-                    textDecoration: textData.textDecoration,
-                    textAlign: textData.textAlign,
-                    justifyContent: textData.textAlign,
-                    letterSpacing: textData.letterSpacing,
-                    lineHeight: textData.lineHeight,
-                    fontFamily: textData.fontFamily,
-                    textTransform: textData.textTransform,
-                    whiteSpace: textData.whiteSpace,
-                    wordBreak: textData.wordBreak,
-                }}>
-                    {pageNumbHelper && type && type === 'PageNumber' ? pageNumbHelper : textData.content}
-                </p>
-            )}
-        </div>
-    );
+  const handleFocus = () => {
+    onFocus();
+    setFocused(true);
+  };
+
+  useEffect(() => {
+    if (isEditing === false) {
+      setFocused(false);
+    }
+    if (editableDivRef.current) {
+      onChange(editableDivRef.current.innerHTML);
+    }
+  }, [editableDivRef.current?.innerHTML, isEditing]);
+
+  return (
+    <div style={styles.EditableTextCont}>
+      <p
+        style={{
+          ...styles.number,
+          display: type !== "List" ? "none" : "",
+          fontSize: textData.fontSize,
+          fontWeight: textData.fontWeight,
+          fontStyle: textData.fontStyle,
+          color: textData.color,
+          textDecoration: textData.textDecoration,
+          textAlign: textData.textAlign,
+          justifyContent: textData.textAlign,
+          letterSpacing: textData.letterSpacing,
+          lineHeight: textData.lineHeight,
+          fontFamily: textData.fontFamily,
+          textTransform: textData.textTransform,
+          whiteSpace: textData.whiteSpace,
+          wordBreak: textData.wordBreak,
+        }}
+      >
+        {order ? `${order}-` : type === "List" && !order ? "-" : ""}{" "}
+      </p>
+      {type !== "PageNumber" && (
+        <div
+          ref={editableDivRef}
+          contentEditable={isEditing}
+          onFocus={handleFocus}
+          onBlur={() => setFocused(false)}
+          style={{
+            ...styles.contentEditableDiv,
+            fontSize: textData.fontSize,
+            fontWeight: textData.fontWeight,
+            fontStyle: textData.fontStyle,
+            color: textData.color,
+            textDecoration: textData.textDecoration,
+            textAlign: textData.textAlign,
+            justifyContent: textData.textAlign,
+            letterSpacing: textData.letterSpacing,
+            lineHeight: textData.lineHeight,
+            fontFamily: textData.fontFamily,
+            textTransform: textData.textTransform,
+            whiteSpace: textData.whiteSpace,
+            wordBreak: textData.wordBreak,
+            boxShadow: focused ? "inset 0 0 0 1px blue" : "none",
+            backgroundColor: "transparent",
+            cursor: isEditing ? "text" : "pointer",
+          }}
+        ></div>
+      )}
+      {pageNumbHelper && type && type === "PageNumber" && (
+        <p
+          style={{
+            ...styles.pageNumber,
+            fontSize: textData.fontSize,
+            fontWeight: textData.fontWeight,
+            fontStyle: textData.fontStyle,
+            color: textData.color,
+            textDecoration: textData.textDecoration,
+            textAlign: "center",
+            justifyContent: textData.textAlign,
+            letterSpacing: textData.letterSpacing,
+            lineHeight: textData.lineHeight,
+            fontFamily: textData.fontFamily,
+            textTransform: textData.textTransform,
+            whiteSpace: textData.whiteSpace,
+            wordBreak: textData.wordBreak,
+          }}
+        >
+          {pageNumbHelper}
+        </p>
+      )}
+    </div>
+  );
 });
 
 const styles = {
-    EditableTextCont: {
-        alignItems: 'center',
-        display: 'flex',
-        height: '100%',
-        width: '100%'
-    } as React.CSSProperties,
-    number: {
-        padding: '0px',
-        margin: '0px'
-    } as React.CSSProperties,
-    text: {
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        alignItems: 'center',
-        backgroundColor: 'transparent',
-        padding: '0px',
-        margin: '0px'
-    } as React.CSSProperties,
-    textareaStyle: {
-        margin: '0px',
-        padding: '0px',
-        width: '100%',
-        border: 'none',
-        outline: 'none',
-        resize: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        overflow: 'hidden',
-        boxSizing: 'border-box',
-        backgroundColor: 'transparent',
-    } as React.CSSProperties,
+  EditableTextCont: {
+    alignItems: "center",
+    display: "flex",
+    height: "100%",
+    width: "100%",
+  } as React.CSSProperties,
+  number: {
+    padding: "0px",
+    margin: "0px",
+  } as React.CSSProperties,
+  contentEditableDiv: {
+    width: "100%",
+    border: "none",
+    outline: "none",
+    overflow: "hidden",
+    boxSizing: "border-box",
+    backgroundColor: "transparent",
+  } as React.CSSProperties,
+  pageNumber: {
+    width: "100%",
+  } as React.CSSProperties,
 };
